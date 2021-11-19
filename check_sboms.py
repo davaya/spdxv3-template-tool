536bea19faa95ef897d733c724df414b5e30f26b
David Kemp <19152940+davaya@users.noreply.github.com>
2021-11-19 08:28:46 -0500

    start split/join

--- check_sboms.py
+++ check_sboms.py
@@ -4,9 +4,28 @@ Validate serialized SPDXv3 files against schema
 import jadn
 import json
 import os
+import re
+from urllib.parse import urlparse, urlunparse
 
 SCHEMA = 'Schemas/spdx-v3.jidl'
 DATA_DIR = 'Data3'
+OUT_DIR = 'Out'
+
+
+def make_iri(eid: str, cx: dict) -> str:
+    u = urlparse(eid)
+    if scheme := u.scheme:
+        if prefix := cx.get('prefixes', {}):
+            u.scheme = cx['prefixes'][prefix]
+            return urlunparse(u)
+    return urlunparse(u)
+
+    print(eid, u)
+
+
+def split_collection(e: dict, outfile) -> None:
+    cx = e.pop('context')
+    oe = {'id': make_iri(e['id'], cx), 'type': e['type']}
 
 
 def load_any(path: str) -> (dict, None):
@@ -32,3 +51,4 @@ if __name__ == '__main__':
         print(f)
         data = json.load(open(os.path.join(DATA_DIR, f)))
         d = sc.decode('Element', data)
+        split_collection(d, os.path.join(OUT_DIR, f))
