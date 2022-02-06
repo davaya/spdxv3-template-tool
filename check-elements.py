7335290508b2e40146ac64fe84d148101e90e932
David Kemp <19152940+davaya@users.noreply.github.com>
2022-02-06 16:38:40 -0500

    Update check script

--- check-elements.py
+++ check-elements.py
@@ -21,10 +21,10 @@ def expand_iri(context: dict, element_id: str) -> str:
     if context:
         u = urlparse(element_id)
         if u.scheme:
-            if prefix := context.get('prefixes', {}).get(u.scheme, ''):
+            if prefix := context['prefixes'].get(u.scheme, ''):
                 return prefix + u.path
             return element_id
-        if element_id not in context.get('local_ids', []):
+        if element_id not in context.get('doc_ids', []):
             print(f'    Undefined Element: {element_id}')
         return context.get('namespace', '') + element_id
     return element_id
@@ -32,7 +32,7 @@ def expand_iri(context: dict, element_id: str) -> str:
 
 def compress_iri(context: dict, iri: str) -> str:
     """
-    Convert an Element IRI to namespace:local form
+    Convert an Element ID IRI to namespace:local form
     """
     if context:
         if base := context.get('namespace', ''):
@@ -46,41 +46,42 @@ def compress_iri(context: dict, iri: str) -> str:
 
 def expand_ids(context: dict, element: dict, paths: list) -> None:
     """
-    Convert all IRIs in element from namespace:local form to absolute IRI
+    Convert all IRIs in an element from namespace:local form to absolute IRI
 
-    Hardcode IRI locations for now; replace with path-driven dynamic update
+    Hardcode IRI locations for now; replace with path-driven update
     """
-    etype = element['type']
     element.update({'id': expand_iri(context, element['id'])})
     if 'created' in element:
         element['created']['by'] = [expand_iri(context, k) for k in element['created']['by']]
-    for t in etype:
-        if 'elements' in etype[t]:
-            etype[t]['elements'] = [expand_iri(context, k) for k in etype[t]['elements']]
-        elif 'originator' in etype[t]:
-            etype[t]['originator'] = [expand_iri(context, k) for k in etype[t]['originator']]
-    if 'annotation' in etype:
-        etype['annotation']['subject'] = expand_iri(context, etype['annotation']['subject'])
-    if 'relationship' in etype:
-        etype['relationship']['from'] = expand_iri(context, etype['relationship']['from'])
-        etype['relationship']['to'] = [expand_iri(context, k) for k in etype['relationship']['to']]
+    for etype, eprops in element['type'].items():
+        for p in eprops:
+            if p in ('elements', 'rootElements', 'originator', 'members'):
+                eprops[p] = [expand_iri(context, k) for k in eprops[p]]
+        if etype == 'annotation':
+            eprops['subject'] = expand_iri(context, eprops['subject'])
+        elif etype == 'relationship':
+            eprops['from'] = expand_iri(context, eprops['from'])
+            eprops['to'] = [expand_iri(context, k) for k in eprops['to']]
 
 
 def compress_ids(context: dict, element: dict) -> None:
-    etype = element['type']
+    """
+    Convert all IRIs in an element from absolute IRI to namespace:local form
+
+    Hardcode IRI locations for now; replace with path-driven update
+    """
     element.update({'id': compress_iri(context, element['id'])})
     if 'created' in element:
         element['created']['by'] = [compress_iri(context, k) for k in element['created']['by']]
-    for t in etype:
-        if 'elements' in etype[t]:
-            etype[t]['elements'] = [compress_iri(context, k) for k in etype[t]['elements']]
-        elif 'originator' in etype[t]:
-            etype[t]['originator'] = [compress_iri(context, k) for k in etype[t]['originator']]
-    if 'annotation' in etype:
-        etype['annotation']['subject'] = compress_iri(context, etype['annotation']['subject'])
-    if 'relationship' in etype:
-        etype['relationship']['from'] = compress_iri(context, etype['relationship']['from'])
-        etype['relationship']['to'] = [compress_iri(context, k) for k in etype['relationship']['to']]
+    for etype, eprops in element['type'].items():
+        for p in eprops:
+            if p in ('elements', 'rootElements', 'originator', 'members'):
+                eprops[p] = [compress_iri(context, k) for k in eprops[p]]
+        if etype == 'annotation':
+            eprops['subject'] = compress_iri(context, eprops['subject'])
+        elif etype == 'relationship':
+            eprops['from'] = compress_iri(context, eprops['from'])
+            eprops['to'] = [compress_iri(context, k) for k in eprops['to']]
 
 
 def expand_element(context: dict, element: dict) -> dict:
@@ -115,10 +116,19 @@ def load_any(path: str) -> (dict, None):
     return loader(path)
 
 
-def dump_elements(document: dict, elements: list[dict]) -> None:
-    print('Document:', {k: v for k, v in document.items() if k != 'elements'})
+def dump_elements(context: dict, elements: list[dict]) -> None:
+    print('Context:', context)
     for e in elements:
-        print(compress_element(document, e))
+        print(compress_element(context, e))
+
+
+def validate_document(document: dict) -> dict:
+    cx = {k: document[k] for k in document if k not in ('elements', 'documentRefs', 'namespaceMap')}
+    cx['doc_ids'] = [compress_iri(cx, e['id']) for e in document['elements']]
+    cx['ref_ids'] = [ref['namespace'] + ':' + e for ref in document.get('documentRefs', []) for e in ref['elements']]
+    cx['prefixes'] = {prefix: uri for uri, prefix in document.get('namespaceMap', {}).items()}
+    # Check defined vs copied namespaces and timestamps
+    return cx
 
 
 def make_dot(context: dict, elist: list, fp: str) -> None:
@@ -126,9 +136,9 @@ def make_dot(context: dict, elist: list, fp: str) -> None:
     with open(os.path.splitext(fp)[0] + '.dot', 'w') as fx:
         fx.write('digraph G {\nnode [fontname=Arial, fontsize=8, shape=box, style=filled, fillcolor=lightskyblue1]\n')
         for e in elist:
-            id = compress_iri(context, e['id'])
-            # print(f"  n{ex[e['id']]}: {id}: {e.get('name', id)}")
-            fx.write(f"n{ex[e['id']]} [label=\"{id}\\n{e.get('name', '')}\"]\n")
+            eid = compress_iri(context, e['id'])
+            # print(f"  n{ex[e['id']]}: {eid}: {e.get('name', eid)}")
+            fx.write(f"n{ex[e['id']]} [label=\"{eid}\\n{e.get('name', '')}\"]\n")
             for t in e['type']:
                 for n in e['type'][t].get('elements', []):
                     dest = f'n{ex[n]}' if n in ex else f'"{compress_iri(context, n)}"'
@@ -142,11 +152,11 @@ if __name__ == '__main__':
     s = load_any(SCHEMA)
     sc = jadn.codec.Codec(s, verbose_rec=True, verbose_str=True)
     for f in os.scandir(DATA_DIR):
-        print(f.name)
         if not f.is_file() or os.path.splitext(f)[1] not in ('.json'):
             continue
+        print(f'  === {f.name}')
         doc = sc.decode('UnitOfTransfer', json.load(open(f.path)))
-        doc['local_ids'] = [compress_iri(doc, e['id']) for e in doc['elements']]
-        x_elements = [expand_element(doc, e) for e in doc['elements']]
-        dump_elements(doc, x_elements)
-        make_dot(doc, x_elements, os.path.join(OUT_DIR, f.name))
+        ctx = validate_document(doc)
+        x_elements = [expand_element(ctx, e) for e in doc['elements']]
+        dump_elements(ctx, x_elements)
+        make_dot(ctx, x_elements, os.path.join(OUT_DIR, f.name))
